dcefa071d94e0574552fe117ae375922ec3cb7e3
Atílio Moreira <kari.moreira@cs.cruzeirodosul.edu.br>
2026-01-02 18:29:59 -0300

    feat: font-face

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,6 @@ import { FormspreeProvider } from '@formspree/react'
 import App from './App.jsx'
 import './index.css' 
 
-import '@fontsource/fira-code/300.css';
-import '@fontsource/fira-code/500.css';
-import '@fontsource/inter/300.css';
-import '@fontsource/inter/600.css';
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <FormspreeProvider>
