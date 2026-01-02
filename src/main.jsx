b342965f13b2b1f1b47011ac2e1d17c6b962ef1f
Atílio Moreira <kari.moreira@cs.cruzeirodosul.edu.br>
2026-01-02 18:12:45 -0300

    feat: fonts locais

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,14 @@ import { FormspreeProvider } from '@formspree/react'
 import App from './App.jsx'
 import './index.css' 
 
+import '@fontsource/fira-code/300.css';
+import '@fontsource/fira-code/400.css';
+import '@fontsource/fira-code/500.css';
+import '@fontsource/inter/300.css';
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/600.css';
+import '@fontsource/inter/700.css';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <FormspreeProvider>
