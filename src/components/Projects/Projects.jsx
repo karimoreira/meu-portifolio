9ef551c03ba499fd2fc3a207586cd76e49fda9b5
Atilio Moreira <kari.moreira@cs.cruzeirodosul.edu.br>
2026-02-25 19:17:26 -0300

    feat: melhoria no efeito 3d

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -13,12 +13,7 @@ function ProjectCard({ project, index }) {
     const rect = card.getBoundingClientRect();
     const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
-    const centerX = rect.width / 2;
-    const centerY = rect.height / 2;
-    const rotateX = ((y - centerY) / centerY) * -8;
-    const rotateY = ((x - centerX) / centerX) * 8;
-
-    card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1.02)`;
+    card.style.transform = 'scale(1.02)';
     card.style.setProperty('--glow-x', `${x}px`);
     card.style.setProperty('--glow-y', `${y}px`);
   }, []);
